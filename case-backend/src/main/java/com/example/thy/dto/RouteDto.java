369a28d665dac3511d1cd89c928e5d497a1a0bdd
hasankzl2 <mhasan.kzl@gmail.com>
2025-03-04 20:23:41 +0300

    swagger update

--- case-backend/src/main/java/com/example/thy/dto/RouteDto.java
+++ case-backend/src/main/java/com/example/thy/dto/RouteDto.java
@@ -2,6 +2,7 @@ package com.example.thy.dto;
 
 import com.example.thy.enums.TransferTypeEnum;
 import com.example.thy.enums.TransportationTypeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -13,14 +14,58 @@ import java.util.List;
 @Getter
 public class RouteDto {
 
+    @Schema(
+            description = "incremented value starts from 1",
+            name = "id",
+            type = "integer",
+            example = "1")
     private int id;
+    @Schema(
+            description = "list of locations route depending on origin and destination locations",
+            name = "validRoutes",
+            type = "RouteLocationDto")
     private List<RouteLocationDto> validRoutes = new ArrayList<>();
+    @Schema(
+            description = "flight Transfer Count",
+            name = "flightTransferCount",
+            type = "integer",
+            example = "1")
     private int flightTransferCount = 0;
+    @Schema(
+            description = "after Flight Transfer Count",
+            name = "afterFlightTransferCount",
+            type = "integer",
+            example = "1")
     private int afterFlightTransferCount = 0;
+    @Schema(
+            description = "before Flight Transfer Count",
+            name = "beforeFlightTransferCount",
+            type = "integer",
+            example = "1")
     private int beforeFlightTransferCount = 0;
+    @Schema(
+            description = "selected departure date",
+            name = "departureDate",
+            type = "LocalDate",
+            example = "2025-03-03")
     private final LocalDate departureDate;
+    @Schema(
+            description = "departure day number (1-7)",
+            name = "departureDay",
+            type = "integer",
+            example = "1")
     private final int departureDay;
+    @Schema(
+            description = "origin location id",
+            name = "startLocationId",
+            type = "integer",
+            example = "1")
     private final Long startLocationId;
+    @Schema(
+            description = "destination location id",
+            name = "endLocationId",
+            type = "integer",
+            example = "1")
     private final Long endLocationId;
 
     public void addValidRoute(LocationDto locationDto, TransportationTypeEnum transportationType, Integer[] operationDays) {
--- case-backend/src/main/java/com/example/thy/dto/RouteLocationDto.java
+++ case-backend/src/main/java/com/example/thy/dto/RouteLocationDto.java
@@ -2,6 +2,7 @@ package com.example.thy.dto;
 
 import com.example.thy.enums.TransferTypeEnum;
 import com.example.thy.enums.TransportationTypeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -9,10 +10,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RouteLocationDto {
 
+    @Schema(
+            description = "location for route",
+            name = "location",
+            type = "LocationDto")
     private LocationDto location;
+    @Schema(
+            description = "Transportation Type",
+            name = "transportationType",
+            type = "TransportationTypeEnum",
+            example = "BUS")
     private TransportationTypeEnum transportationType;
+    @Schema(
+            description = "Transfer Type",
+            name = "transferType",
+            type = "TransferTypeEnum",
+            example = "AFTER_FLIGHT")
     private TransferTypeEnum transferType;
+    @Schema(
+            description = "Operation days",
+            name = "operationDays",
+            type = "Integer[]",
+            example = "1")
     private Integer[] operationDays;
+
     public RouteLocationDto(LocationDto location, TransportationTypeEnum transportationTypeEnum, TransferTypeEnum transferTypeEnum,Integer[] operationDays) {
         this.location = location;
         this.transportationType = transportationTypeEnum;
