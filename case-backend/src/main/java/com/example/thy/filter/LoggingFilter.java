99e0a3ad06de832f3f619409d857090f95a642a3
hasankzl2 <mhasan.kzl@gmail.com>
2025-03-05 13:29:25 +0300

    filter added for logging

---
+++ case-backend/src/main/java/com/example/thy/filter/LoggingFilter.java
@@ -0,0 +1,32 @@
+package com.example.thy.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class LoggingFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        log.info("Incoming Request: {} {}", req.getMethod(), req.getRequestURI());
+
+        // Devam et
+        chain.doFilter(request, response);
+
+        log.info("Outgoing Response: {} {}", res.getStatus(), req.getRequestURI());
+    }
+}
